1460cc084c7970e5821cd407d013145c1df155ed
Romain GABORIEAU <romain.gaborieau@etud.univ-angers.fr>
2015-02-24 16:22:13 +0100

    Unification des documents

--- unify-db.pl
+++ unify-db.pl
@@ -4,15 +4,16 @@ $argc = $#ARGV +2;
 
 if($argc == 0)
 {
-	print "Usage: perl unify-db.pl dirpath [prefix]";
-	print "\tdirpath - path to the directory containing documents";
-	print "\tprefix - optional, the prefix to added to generated documents";
+	print "Usage: perl unify-db.pl dirpath [prefix]\n";
+	print "\tdirpath - path to the directory containing documents\n";
+	print "\tprefix - optional, the prefix to added to generated documents\n";
+	exit(0);
 }
 
 if($argc>0)
 {
 	$path = $ARGV[0];
-	if(substr($path, -1) == "/")
+	if(substr($path, -1) eq "/")
 	{
 		chop $path;
 	}
@@ -41,6 +42,7 @@ if(-d "$path/../unifiedDB")
 else
 {
 	mkdir "$path/../unifiedDB";
+	print "Create directory $path/../unifiedDB\n";
 }
 
 foreach $file (@files) {
@@ -64,10 +66,13 @@ foreach $file (@files) {
 			$text = "<?xml version=\"1.0\"?>\n" . $item . "\n</DOC>";
 			open( $fh, ">", "$path/../unifiedDB/$prefix$i.xml") or die "Could not open file : $!";
 			print $fh "$text";
+			print "\tUnify document #$i\n";
 			$i++;
 		}
 
 	} else {
 	  	warn "Could not open file '$file' $!";
 	}
+
+	print "\n$i documents unified\n";
 }
